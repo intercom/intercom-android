5a9ec90e909fc2c088f7ca49efbab90467364d30
Sean Marnane <34036046+kraftysm@users.noreply.github.com>
2022-01-19 15:18:37 +0000

    Updates to new sample app (#563)
    * Add register button to sample app
    
    * Add launcher toggle button to sample
    
    * Unregister user and add user group

--- sample/app/src/main/java/com/intercom/sample/MainActivity.kt
+++ sample/app/src/main/java/com/intercom/sample/MainActivity.kt
@@ -1,26 +1,47 @@
 package com.intercom.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 import io.intercom.android.sdk.Intercom
 import io.intercom.android.sdk.identity.Registration
-import io.intercom.android.sdk.push.IntercomPushClient
 
 class MainActivity : AppCompatActivity() {
+    var launcherVisibility = false
+    val registerBtn by lazy { findViewById<Button>(R.id.btn_register) }
+    val launcherVisibiltyBtn by lazy { findViewById<Button>(R.id.btn_toggle_launcher_visibility) }
+    val unregisterUserBtn by lazy {findViewById<Button>(R.id.btn_unregister_user)}
+    val userGroup by lazy { findViewById<Group>(R.id.group_user)}
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(API_KEY.isBlank() or APP_ID.isBlank()){
-            findViewById<TextView>(R.id.textview).text = "Please set App ID and Api Key in SampleApplication.kt"
-        }else{
-            val registration = Registration.create().withUserId("1234567")
-            Intercom.client().registerIdentifiedUser(registration)
-            Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+        registerBtn.setOnClickListener {
+            if (API_KEY.isBlank() or APP_ID.isBlank()) {
+                Toast.makeText(this, "Please fix this!", Toast.LENGTH_SHORT)
+            } else {
+                val registration = Registration.create().withUserId("123")
+                Intercom.client().registerIdentifiedUser(registration)
+                Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+                userGroup.visibility = View.VISIBLE
+                launcherVisibility = true
+            }
+        }
+        launcherVisibiltyBtn.setOnClickListener {
+            if (launcherVisibility)
+                Intercom.client().setLauncherVisibility(Intercom.GONE)
+            else
+                Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+            launcherVisibility = launcherVisibility.not()
+        }
 
-            findViewById<TextView>(R.id.textview).text = "Intercom initialised successfully"
+        unregisterUserBtn.setOnClickListener {
+            Intercom.client().logout()
+            userGroup.visibility = View.GONE
         }
     }
 }
