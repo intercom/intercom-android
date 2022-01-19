5754aced1446793c5d50172bcbc55ddd330a0152
kraftysm <sean.marnane@intercom.io>
2022-01-19 14:41:11 +0000

    Add register button to sample app

--- sample/app/src/main/java/com/intercom/sample/MainActivity.kt
+++ sample/app/src/main/java/com/intercom/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.intercom.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import io.intercom.android.sdk.Intercom
@@ -9,18 +10,20 @@ import io.intercom.android.sdk.identity.Registration
 import io.intercom.android.sdk.push.IntercomPushClient
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if(API_KEY.isBlank() or APP_ID.isBlank()){
-            findViewById<TextView>(R.id.textview).text = "Please set App ID and Api Key in SampleApplication.kt"
-        }else{
-            val registration = Registration.create().withUserId("1234567")
-            Intercom.client().registerIdentifiedUser(registration)
-            Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
-
-            findViewById<TextView>(R.id.textview).text = "Intercom initialised successfully"
+        val registerBtn = findViewById<Button>(R.id.btn_register)
+        registerBtn.setOnClickListener {
+            if (API_KEY.isBlank() or APP_ID.isBlank()) {
+                Toast.makeText(this, "Please fix this!", Toast.LENGTH_SHORT)
+            } else {
+                val registration = Registration.create().withUserId("123")
+                Intercom.client().registerIdentifiedUser(registration)
+                Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+            }
         }
     }
 }
