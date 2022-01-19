09753737902859a68c1b2d6f9b8049970ec70015
kraftysm <sean.marnane@intercom.io>
2022-01-19 15:02:15 +0000

    Add launcher toggle button to sample

--- sample/app/src/main/java/com/intercom/sample/MainActivity.kt
+++ sample/app/src/main/java/com/intercom/sample/MainActivity.kt
@@ -1,21 +1,22 @@
 package com.intercom.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import io.intercom.android.sdk.Intercom
 import io.intercom.android.sdk.identity.Registration
-import io.intercom.android.sdk.push.IntercomPushClient
 
 class MainActivity : AppCompatActivity() {
+    var launcherVisibility = false
+    val registerBtn by lazy { findViewById<Button>(R.id.btn_register) }
+    val launcherVisibiltyBtn by lazy { findViewById<Button>(R.id.btn_toggle_launcher_visibility) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val registerBtn = findViewById<Button>(R.id.btn_register)
         registerBtn.setOnClickListener {
             if (API_KEY.isBlank() or APP_ID.isBlank()) {
                 Toast.makeText(this, "Please fix this!", Toast.LENGTH_SHORT)
@@ -23,7 +24,16 @@ class MainActivity : AppCompatActivity() {
                 val registration = Registration.create().withUserId("123")
                 Intercom.client().registerIdentifiedUser(registration)
                 Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+                launcherVisibiltyBtn.visibility = View.VISIBLE
+                launcherVisibility = true
             }
+        }
+        launcherVisibiltyBtn.setOnClickListener {
+            if (launcherVisibility)
+                Intercom.client().setLauncherVisibility(Intercom.GONE)
+            else
+                Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+            launcherVisibility = launcherVisibility.not()
         }
     }
 }
