95a17b4d824617dd1ed0df3fb4bd73caa645520b
kraftysm <sean.marnane@intercom.io>
2022-01-21 14:26:24 +0000

    Updated UI and refactored Toast message

--- sample/app/src/main/java/com/intercom/sample/MainActivity.kt
+++ sample/app/src/main/java/com/intercom/sample/MainActivity.kt
@@ -11,23 +11,40 @@ import io.intercom.android.sdk.identity.Registration
 
 class MainActivity : AppCompatActivity() {
     var launcherVisibility = false
-    val registerBtn by lazy { findViewById<Button>(R.id.btn_register) }
+    val regIdentifiedBtn by lazy { findViewById<Button>(R.id.btn_register_identified) }
+    val regUnidentifiedBtn by lazy { findViewById<Button>(R.id.btn_register_unidentified) }
     val launcherVisibiltyBtn by lazy { findViewById<Button>(R.id.btn_toggle_launcher_visibility) }
     val unregisterUserBtn by lazy {findViewById<Button>(R.id.btn_unregister_user)}
     val userGroup by lazy { findViewById<Group>(R.id.group_user)}
+
+    private fun validateAPIKey() : Boolean {
+        if (API_KEY.isBlank() or APP_ID.isBlank()) {
+            Toast.makeText(this, "Please update your API Key and App ID in SampleApplication!", Toast.LENGTH_SHORT).show()
+            return false
+        }
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        registerBtn.setOnClickListener {
-            if (API_KEY.isBlank() or APP_ID.isBlank()) {
-                Toast.makeText(this, "Please fix this!", Toast.LENGTH_SHORT)
-            } else {
+        regUnidentifiedBtn.setOnClickListener {
+            if(validateAPIKey()) {
+                Intercom.client().registerUnidentifiedUser()
+                Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+                userGroup.visibility = View.VISIBLE
+                launcherVisibility = true
+            }
+        }
+
+        regIdentifiedBtn.setOnClickListener {
+            if(validateAPIKey()) {
                 val registration = Registration.create().withUserId("123")
                 Intercom.client().registerIdentifiedUser(registration)
                 Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
                 userGroup.visibility = View.VISIBLE
+                regUnidentifiedBtn.visibility = View.GONE
                 launcherVisibility = true
             }
         }
@@ -42,6 +59,9 @@ class MainActivity : AppCompatActivity() {
         unregisterUserBtn.setOnClickListener {
             Intercom.client().logout()
             userGroup.visibility = View.GONE
+            regUnidentifiedBtn.visibility = View.VISIBLE
         }
+
     }
+
 }
