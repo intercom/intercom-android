364ed720d658c8a4db60ce248cd4def5be3bf5fb
kraftysm <sean.marnane@intercom.io>
2022-03-10 23:32:06 +0100

    Add datastore preferences to manage logged-in status and launcher visibility.

--- sample/app/src/main/java/com/intercom/sample/screens/HomeScreen.kt
+++ sample/app/src/main/java/com/intercom/sample/screens/HomeScreen.kt
@@ -2,11 +2,8 @@ package com.intercom.sample.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
-import com.google.android.gms.common.api.internal.StatusCallback
 import com.intercom.sample.components.InputPanel
 import com.intercom.sample.components.SelfServe
 import com.intercom.sample.components.UserUpdate
@@ -18,23 +15,24 @@ import io.intercom.android.sdk.identity.Registration
 
 @Preview(device = Devices.PIXEL_4)
 @Composable
-fun HomeScreen(onUserEditorLaunched: () -> Unit = {}) {
-    val showRegistrationDetails = rememberSaveable { mutableStateOf(false) }
+fun HomeScreen(
+    onUserEditorLaunched: () -> Unit = {},
+    currentRegistrationStatus: Boolean = false,
+    toggleRegistrationStatus: (Boolean) -> Unit = {}
+) {
+
     Column {
         InputPanel(
             onRegisterClicked = { uniqueId: String, hasUserId: Boolean ->
-                if (hasUserId) {
+                if (hasUserId)
                     Intercom.client().loginIdentifiedUser(userRegistration = Registration().withUserId(uniqueId))
-                }
-                else {
+                else
                     Intercom.client().loginIdentifiedUser(userRegistration = Registration().withEmail(uniqueId))
-                }
-                Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
-                showRegistrationDetails.value = true
+                toggleRegistrationStatus.invoke(true)
             },
             onUnregisterClicked = {
                 Intercom.client().logout()
-                showRegistrationDetails.value = false
+                toggleRegistrationStatus(false)
             },
             onRegisterUnidentifiedClicked = {
                 Intercom.client().loginUnidentifiedUser(
@@ -50,10 +48,11 @@ fun HomeScreen(onUserEditorLaunched: () -> Unit = {}) {
                         }
                     }
                 )
+                toggleRegistrationStatus(true)
             }
         )
 
-        if (showRegistrationDetails.value) {
+        if (currentRegistrationStatus) {
             UserUpdate(
                 onUserUpdateClicked = { onUserEditorLaunched() }
             )
@@ -68,6 +67,10 @@ fun HomeScreen(onUserEditorLaunched: () -> Unit = {}) {
                     Intercom.client().displayCarousel(it)
                 }
             )
+            Intercom.client().setLauncherVisibility(Intercom.VISIBLE)
+        }
+        else {
+            Intercom.client().setLauncherVisibility(Intercom.GONE)
         }
     }
 }
