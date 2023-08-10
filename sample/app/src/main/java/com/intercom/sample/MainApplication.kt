bc27bc53f621fc2061aa7efa48f15fb5d079a643
Artyom Shendrik <amal.samally@gmail.com>
2023-08-10 11:58:23 +0200

    FIXME: Causes IllegalAccessException: Tried to access the API:LayoutInflater which needs to have proper configuration from a non-UI Context

--- sample/app/src/main/java/com/intercom/sample/MainApplication.kt
+++ sample/app/src/main/java/com/intercom/sample/MainApplication.kt
@@ -6,6 +6,8 @@ import io.intercom.android.sdk.Intercom
 class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        // FIXME: Causes IllegalAccessException: Tried to access the API:LayoutInflater which needs to have proper configuration from a non-UI Context
         Intercom.initialize(this, "", "")
     }
 }
