18215f79a21f0214fe422102db4b2854238aa203
Irena Szukala <irena.szukala@intercom.consulting>
2026-03-31 12:10:42 +0200

    Updated Sample app

--- sample/app/src/main/java/com/intercom/sample/components/UserDetailsEditor.kt
+++ sample/app/src/main/java/com/intercom/sample/components/UserDetailsEditor.kt
@@ -9,16 +9,16 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.intercom.sample.R
 import io.intercom.android.sdk.UserAttributes
 
 @Preview
@@ -48,7 +48,7 @@ fun UserDetailsEditor(onUserDetailsSaved: (UserAttributes) -> Unit = {}) {
                             onClick = { userStates.remove(userStates[index]) },
                             content = {
                                 Icon(
-                                    imageVector = Icons.Outlined.Delete,
+                                    painter = painterResource(R.drawable.intercom_ic_delete),
                                     contentDescription = "Delete row"
                                 )
                             }
